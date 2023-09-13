81e905d83e8e9c9fecd2e2179ced64896f212ae9
Peter Osoo <posoo617@gmail.com>
2023-09-13 15:08:39 +0300

    app - counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,13 +3,22 @@ import "../App.css"
 
 const Counter = () => {
 	const [count, setCount] = useState(0)
+	const increment = () => {
+		setCount(count + 1)
+	}
+	const decrement = () => {
+		setCount(count - 1)
+	}
+	const reset = () => {
+		setCount(0)
+	}
 	return (
-		<div>
+		<div className="App">
 			<h2>Counter</h2>
 			<p>Count : {count}</p>
-			<button onClick={() => setCount(count + 1)}>Increment</button>
-			<button onClick={() => setCount(count - 1)}>Decrement</button>
-			<button onClick={() => setCount(0)}>Reset</button>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+			<button onClick={reset}>Reset</button>
 		</div>
 	)
 }
