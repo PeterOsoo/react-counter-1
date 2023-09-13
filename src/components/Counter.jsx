10183919b4a3547db5ea778258fc4fa9ca5cbdb7
Peter Osoo <posoo617@gmail.com>
2023-09-13 15:13:47 +0300

    styles

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -16,9 +16,15 @@ const Counter = () => {
 		<div className="App">
 			<h2>Counter</h2>
 			<p>Count : {count}</p>
-			<button onClick={increment}>Increment</button>
-			<button onClick={decrement}>Decrement</button>
-			<button onClick={reset}>Reset</button>
+			<button onClick={increment} className="btn btn-primary">
+				Increment
+			</button>
+			<button onClick={decrement} className="btn btn-danger">
+				Decrement
+			</button>
+			<button onClick={reset} className="btn btn-success">
+				Reset
+			</button>
 		</div>
 	)
 }
